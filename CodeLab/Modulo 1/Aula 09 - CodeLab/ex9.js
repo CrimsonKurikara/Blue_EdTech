070f2aee2582168a7565987c386a3e34c7b3f95b
CrimsonKurikara <gabriel_silva.2016@outlook.com>
2021-10-01 19:27:26 -0300

    retirado o console log

--- CodeLab/Modulo 1/Aula 09 - CodeLab/ex9.js
+++ CodeLab/Modulo 1/Aula 09 - CodeLab/ex9.js
@@ -3,7 +3,6 @@ var fibonacci = [0,1];
 for(let i = fibonacci.length; i<10; i++)
 {
     fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
-    console.log(i + " = " + (i-1) + " + " + (i-2));
 }
 console.log();
 console.log(fibonacci);
